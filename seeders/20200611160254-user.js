52e6fe372a5cf88d2e859c1b58f7be8ab16d81c7
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2020-06-12 17:42:29 +0700

    add seeder user

--- seeders/20200611160254-user.js
+++ seeders/20200611160254-user.js
@@ -1,19 +1,42 @@
 'use strict'
 const faker = require('faker')
 
-const roleId = ['1', '2', '3']
-const userId = ['11', '12', '13', '14', '15', '16', '17', '18', '19', '10']
-
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    const fakeData = Array.from({ length: 10 }).map((data, index) => {
-      return {
-        id: userId[index],
+    // const fakeData = Array.from({ length: 10 }).map((data, index) => {
+    //   return {
+    //     id: userId[index],
+    //     username: faker.internet.userName(),
+    //     password: faker.internet.password(),
+    //     roleId: roleId[Math.floor(Math.random() * 2)]
+    //   }
+    // })
+    const fakeData = [
+      {
+        id: '123',
         username: faker.internet.userName(),
-        password: faker.internet.password(),
-        roleId: roleId[Math.floor(Math.random() * 2)]
+        password: '12345',
+        roleId: '1'
+      },
+      {
+        id: '1235',
+        username: faker.internet.userName(),
+        password: '12345',
+        roleId: '2'
+      },
+      {
+        id: '1234',
+        username: faker.internet.userName(),
+        password: '12345',
+        roleId: '3'
+      },
+      {
+        id: '12345',
+        username: faker.internet.userName(),
+        password: '12345',
+        roleId: '2'
       }
-    })
+    ]
     return queryInterface.bulkInsert('Users', fakeData, {})
   },
 
