8a52e260e43cf8ce55d62bbc0a4a80e79a3ded7d
Adi Fatkhurozi <ady.fatkhurozi@gmail.com>
2020-06-18 13:37:58 +0700

    add address api

---
+++ routes/addressApi/index.js
@@ -0,0 +1,92 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import {
+  ReqException,
+  checkErrorRequest
+} from '../../utils/exception'
+import request from 'request'
+import auth from '../../middleware/auth'
+
+dotenv.config()
+const router = express.Router()
+
+router.get('/province', auth, async (req, res) => {
+  try {
+    const configs = {
+      url: 'https://dev.farizdotid.com/api/daerahindonesia/provinsi',
+      headers: { key: process.env.API_KEY_RAJAONGKIR }
+    }
+    request(configs, (error, response, body) => {
+      if (error) {
+        throw ReqException({ status: 404, data: [], message: 'Province not Found' })
+      }
+      const result = JSON.parse(body)
+      res.json({
+        data: result.provinsi
+      })
+    })
+  } catch (err) {
+    res.status(err.status || 500)
+    res.json({
+      data: [],
+      message: err.message || 'Gagal mengambil bedroom',
+      messageSystem: checkErrorRequest(err)
+    })
+  }
+})
+
+router.get('/dorp', auth, async (req, res) => {
+  try {
+    const configs = {
+      url: 'https://dev.farizdotid.com/api/daerahindonesia/kota',
+      qs: {
+        id_provinsi: req.query.provinceId
+      }
+    }
+    request(configs, (error, response, body) => {
+      if (error) {
+        throw ReqException({ status: 404, data: [], message: 'Province not Found' })
+      }
+      const result = JSON.parse(body)
+      res.json({
+        data: result.kota_kabupaten
+      })
+    })
+  } catch (err) {
+    res.status(err.status || 500)
+    res.json({
+      data: [],
+      message: err.message || 'Gagal mengambil bedroom',
+      messageSystem: checkErrorRequest(err)
+    })
+  }
+})
+
+router.get('/district', auth, async (req, res) => {
+  try {
+    const configs = {
+      url: 'https://dev.farizdotid.com/api/daerahindonesia/kecamatan',
+      qs: {
+        id_kota: req.query.dorpId
+      }
+    }
+    request(configs, (error, response, body) => {
+      if (error) {
+        throw ReqException({ status: 404, data: [], message: 'Province not Found' })
+      }
+      const result = JSON.parse(body)
+      res.json({
+        data: result
+      })
+    })
+  } catch (err) {
+    res.status(err.status || 500)
+    res.json({
+      data: [],
+      message: err.message || 'Gagal mengambil bedroom',
+      messageSystem: checkErrorRequest(err)
+    })
+  }
+})
+
+export default router
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ import userApi from './userApi'
 import generationApi from './generationApi'
 import bedroomApi from './bedroomApi'
 import classApi from './classApi'
+import addressApi from './addressApi'
 
 export default function (app) {
   app.use('/user', userApi)
   app.use('/class', classApi)
   app.use('/generation', generationApi)
   app.use('/bedroom', bedroomApi)
+  app.use('/address', addressApi)
 }
